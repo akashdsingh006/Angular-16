8ac557c0db27ff5aef9d078942bff49a9d76fb72
Akash Anand <82928605+akashdsingh006@users.noreply.github.com>
2023-09-24 20:19:01 +0530

    Add files via upload

---
+++ Pass Data from Child to Parent/name/name.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-name',
+  templateUrl: './name.component.html',
+  styles: [
+    `.custom{
+      background-color : green;
+    }`
+  ]
+})
+export class NameComponent {
+  my_data ="Hello Sir";
+
+  color='orange'
+}
