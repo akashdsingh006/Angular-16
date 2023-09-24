bc4a02a0f5d20ca79f61616a67a9804c7cbe05b6
Akash Anand <82928605+akashdsingh006@users.noreply.github.com>
2023-09-24 12:41:26 +0530

    Create Child.ts

---
+++ Pass Data from parent to child component/Child.ts
@@ -0,0 +1,12 @@
+import { Component,Input } from '@angular/core';
+
+@Component({
+  selector: 'app-to-do-list',
+  templateUrl: './to-do-list.component.html',
+  styleUrls: ['./to-do-list.component.css']
+})
+export class ToDoListComponent {
+ 
+
+  @Input() item=0;
+}
